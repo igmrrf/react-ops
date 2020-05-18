46bb9f910fca6ca9831fe49a62c650cbf9569651
igmrrf <francis.igbiriki.fi@gmail.com>
2020-05-19 00:25:52 +0100

    Simple React Boiler Plate

---
+++ README.md
@@ -0,0 +1,23 @@
+# React From Scratch
+
+The simplest react boiler plate for your project.
+
+
+
+## Dependencies
+
+### React
+### React-dom
+### React-script
+
+
+
+## Project Map
+
+-public
+    -index.html
+-src
+    -index.js
+package.json
+README.md
+yarn.lock
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>React from Scratch in 5 mins</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
