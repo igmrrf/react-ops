e3c1cc1ada902458c56589fb3cbbc0205fc5a28d
igmrrf <francis.igbiriki.fi@gmail.com>
2020-08-22 22:16:21 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import Delete from "@material-ui/icons/Delete";
+import WithStyles from "@material-ui/styles/withStyles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = { root: { margin: 20, padding: 20, maxWidth: 400 } };
+export default class App extends Component {
+  state = {
+    exercises: [
+      { id: 1, title: "Bench Press" },
+      { id: 2, title: "Deadlift" },
+      { id: 3, title: "Squats" }
+    ],
+    title: ""
+  };
+  handleDelete = id => {
+    console.log(id);
+    this.setState(({ exercises }) => ({
+      exercises: exercises.filter(ex => ex.id !== id)
+    }));
+  };
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({ [name]: value });
+  handleCreate = e => {
+    e.preventDefault();
+    if (this.state.title) {
+      this.setState(({ exercises, title }) => ({
+        exercises: [...exercises, { title, id: Date.now() }],
+        title: ""
+      }));
+    }
+    console.log("Yamero");
+  };
+
+  render() {
+    const { title, exercises } = this.state;
+
+    return (
+      <Paper>
+        <Typography variant="h3" component="h1" align="center" gutterBottom>
+          Exercises
+        </Typography>
+        <form onSubmit={this.handleCreate}>
+          <TextField
+            name="title"
+            label="Exercises"
+            onChange={this.handleChange}
+            value={title}
+            margin="normal"
+          />
+          <Button variant="contained" type="submit" color="primary">
+            Create
+          </Button>
+        </form>
+        <List>
+          {exercises.map(({ id, title }) => (
+            <ListItem key={id}>
+              <ListItemText primary={title} />
+              <ListItemSecondaryAction>
+                <IconButton
+                  color="primary"
+                  onClick={() => this.handleDelete(id)}
+                >
+                  <Delete />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    );
+  }
 }
-
-export default App;
